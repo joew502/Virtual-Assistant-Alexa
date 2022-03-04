4847ca71e18b923e35de87f2ae211b80021f7f0e
Joe Wilson <joewilson502@gmail.com>
2022-03-04 16:27:23 +0000

    Initial Alpha Code import and changes

---
+++ Alpha/alpha.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	APPID = "G469T3-267PUX7QWR"
+	URI   = "http://api.wolframalpha.com/v1/result?appid=" + APPID
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func Alpha(w http.ResponseWriter, r *http.Request) {
+	t := map[string]interface{}{}
+	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
+		if textIn, ok := t["text"].(string); ok {
+			if textOut, err := AlphaService(textIn); err == nil {
+				u := map[string]interface{}{"text": textOut}
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(u)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func AlphaService(text string) (string, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", URI, nil)
+	check(err)
+
+	rsp, err2 := client.Do(req)
+	check(err2)
+
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode == http.StatusOK {
+		body, err3 := ioutil.ReadAll(rsp.Body)
+		check(err3)
+		return string(body), nil
+	} else {
+		return "", errors.New("cannot convert to speech to text")
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/alpha", Alpha).Methods("POST")
+	http.ListenAndServe(":3001", r)
+}
